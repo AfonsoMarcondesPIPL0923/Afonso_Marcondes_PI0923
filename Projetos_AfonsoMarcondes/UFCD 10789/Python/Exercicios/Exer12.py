ae4c350a86a085b284767d2aaf69b6362694f8b3
AfonsoMarcondesPIPL0923 <afonso.marcondes.t0126443@edu.atec.pt>
2025-04-23 09:24:49 +0100

    Update Exer12.py

--- Projetos_AfonsoMarcondes/UFCD 10789/Python/Exercicios/Exer12.py
+++ Projetos_AfonsoMarcondes/UFCD 10789/Python/Exercicios/Exer12.py
@@ -4,7 +4,7 @@ for i in range(1, num + 1):
     print("\n****", i, "****")
     print(num, "+", i, "=" , num + i)
     print(num, "-", i, "=" , num - i)
-    print(num, "*", i, "=" , num * i)
+    print(num, "x", i, "=" , num * i)
     print(num, "/", i, "=" , num / i)
     cont += 4
 print("\n** O programa fez um todal de", cont, "operações **")
