0451d4fb501feb42e4a48910317dee6ac9fe39c7
AfonsoMarcondesPIPL0923 <afonso.marcondes.t0126443@edu.atec.pt>
2025-04-23 13:54:01 +0100

    Update teste_final.py

--- Projetos_AfonsoMarcondes/UFCD 10789/Python/Exercicios/teste_final.py
+++ Projetos_AfonsoMarcondes/UFCD 10789/Python/Exercicios/teste_final.py
@@ -1,7 +1,9 @@
 def detalhes(num):
-    print()
-    for i in range(num, 1, -1):
+    
+    
+    for i in range(num, 0, -1):
         
+        print(f"******** {i} *****************************")
         # Vê se é um numero perfeito
         lista = []
         for y in range(1, i):
@@ -24,18 +26,76 @@ def detalhes(num):
             if i % y == 0:
                 ls.append(y)
         print("Os divisores de", i, "são:", ls)
-        print()
         
+        print("****************************************")
+        print("")
+        
+        
+        # Pergunta se quer parar
+        if i % 10 == 0:
+            escolha = input("\n[ENTER] para continuar...\n [PARA] para parar: ")
+            if escolha.lower() == 'para':
+                break
 
+# Calculadora Simples
+def calculadora():
+    num1 = int(input("\nInsira o 1º numero: "))
+    
+    while True:
+        ls = ['+', '-', 'x', '/', 'X', '*']
+        opr = input("[+] [-] [x] [/]: ")
+        if not opr in ls:
+            print("Opção Invalida")
+        else:
+            break
 
+    num2 = int(input("Insira o 2º numero: "))
+    
+    match opr:
+        case '+':
+            print(num1, '+', num2, '=', num1 + num2)
+        case '-':
+            print(num1, '-', num2, '=', num1 - num2)
+        case 'x':
+            print(num1, 'x', num2, '=', num1 * num2)
+        case '/':
+            print(num1, '/', num2, '=', num1 / num2)
+
+# Imprime a tabuada do 1 até á tabuada do numero inserido
+def tab():
+    
+    # Garante que o numero seja de 1 a 1000
+    while True:
+        try:
+            num = int(input("Insira um número para a tabuada (1 até 1000): "))
+            if not (1 <= num <= 1000):
+                print("Número fora do intervalo permitido (1 até 1000).")
+                continue
+            break
+        except ValueError:
+            print("Entrada inválida! Insira um número válido.")
+            
+    for i in range(1, num + 1):
+        print(f"\n--- Tabuada do {i} ---")
+        for j in range(1, 11):
+            print(f"{i} x {j} = {i*j}")
+        
+        # Pergunta se quer parar
+        if i % 20 == 0:
+            escolha = input("\n[ENTER] para continuar...\n [PARA] para parar: ")
+            if escolha.lower() == 'para':
+                break
+
+########## Menu Principal ##########
 while True:
     print("\n****************************")
     print("*       **Escolhas**       *")
     print("*   1 - Insira um Numero   *")
     print("*   2 - Calculadora        *")
-    print("*   3 - Sair               *")
+    print("*   3 - Tabuada            *")
+    print("*   4 - Sair               *")
     print("*                          *")
-    print("****************************\n")
+    print("*******************************\n")
     
     escolha = int(input("Insira a sua escolha: "))
     
@@ -45,9 +105,13 @@ while True:
             detalhes(num)
             
         case 2:
-            break
+            calculadora()
             
         case 3:
+            tab()
+            
+        case 4:
+            print("\n**Codigo Encerrado**")
             break
             
         
