18a5ebf4b674acd78024130493707bdb0b3345fc
AfonsoMarcondesPIPL0923 <afonso.marcondes.t0126443@edu.atec.pt>
2025-05-20 14:06:51 +0100

    Update testeFinal2.py

--- Projetos_AfonsoMarcondes/UFCD 10789/Teste Final/testeFinal2.py
+++ Projetos_AfonsoMarcondes/UFCD 10789/Teste Final/testeFinal2.py
@@ -2,36 +2,74 @@ class cliente:
     listaClientes = []
     numClientes = 0
     
-    def __init__(self, Numcli, NomCli, morada, tel, nif, compra, Divfin):
+    def __init__(self, NomCli, morada, tel, nif, compra, Divfin):
         cliente.numClientes += 1
-        self.Numcli = numClientes
+        self.Numcli = cliente.numClientes
         self.NomCli = NomCli
         self.morada = morada
         self.tel = tel
         self.nif = nif
         self.compra = compra
-        self.Divfin = Divfin
+        
+        # Calcular o Desconto
+        if compra > 99 and compra < 201:
+            self.Divfin = cliente.compra * 0.95
+            
+        elif compra > 200 and compra < 500:
+            self.Divfin = cliente.compra * 0.90
+        
+        elif compra >= 500:
+            self.Divfin = cliente.compra * 0.85
+        
+        else:
+            self.Divfin = Divfin
+        
+        # Adiciona à lista
         cliente.listaClientes.append(self)
         
     @classmethod
     def listarClientes(cls):
+        cont = 1
         for i in cls.listaClientes:
-            print(f"**{i+1}º Cliente**")
+            print(f"\n\n|{cont}º Cliente|")
             print(f"Nome: {i.NomCli}")
             print(f"Morada: {i.morada}")
             print(f"Telefone: {i.tel}")
             print(f"NIF: {i.nif}")
             print(f"Valor da Compra (Sem Desconto): {i.compra}")
             print(f"Divida Final (Com Desconto): {i.Divfin}")
-
-
+            print("____________________________________________")
+            cont += 1
+            
+            para = input("[para] para parar, qualquer outra tecla para continuar:")
+            if para == "para" or para == "PARA":
+                break
+    def procurarCliente(cls, num):
+        self.num = num
+        
+        for i in cls.listaClientes:
+            if i.Numcli == num:
+                print("\n\n|Cliente Encontrado|")
+                print("\n____________________________________________")
+                print(f"|{cont}º Cliente|")
+                print(f"Nome: {i.NomCli}")
+                print(f"Morada: {i.morada}")
+                print(f"Telefone: {i.tel}")
+                print(f"NIF: {i.nif}")
+                print(f"Valor da Compra (Sem Desconto): {i.compra}")
+                print(f"Divida Final (Com Desconto): {i.Divfin}")
+                print("____________________________________________")
+                break
+        print("\n\n|Cliente Não Encontrado|")
+        
 while(True):
-    print("******************************")
+    print("\n******************************")
     print("*         |Escolhas|         *")
     print("*                            *")
     print("*    1 - Novo Cliente        *")
     print("*    2 - Listar Clientes     *")
-    print("*    3 - Parar Programa      *")
+    print("*    3 - Procurar Cliente    *")
+    print("*    4 - Parar Programa      *")
     print("*                            *")
     print("******************************")
 
@@ -39,7 +77,7 @@ while(True):
 
     match escolha:
         case 1:
-            print(f"Cadastro do {numClientes + 1}ºN Cliente")
+            print(f"\nCadastro do {cliente.numClientes + 1}ºN Cliente")
             NomCli = input("Nome: ")
             morada = input("Morada: ")
             tel = int(input("Telefone: "))
@@ -50,7 +88,15 @@ while(True):
             p1 = cliente(NomCli, morada, tel, nif, compra, Divfin)
             
         case 2:
-            print("2")
+            print("____________________________________________")
+            cliente.listarClientes()
+            print("\n\n")
+        
         case 3:
+            proc = int(input("Insira o numero no cliente que pretende procurar: "))
+            cliente.procurarCliente(proc)
+            print("\n\n")
+            
+        case 4:
             print("\n**Programa Encerrado**")
             break
